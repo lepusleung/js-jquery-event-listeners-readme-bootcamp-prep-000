14e55f1610b21175315f5d1e76da690c68b9e5ea
Man Leung <lepusleung@gmail.com>
2017-09-21 19:57:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
-//define functions here
-
+function getIt() {
 $(document).ready(function(){
-
-// call functions here
+  if (key.which == /p ){
+    alert("Hey!");
 
 });
+
+function frameIt(){
+  $('img').on('load', function())
+}
+
+function pressIt() {
+  $('form'.on('keydown', function())){
+    if (key.which == "G") {
+      alert('G was pressed');
+    }
+  }
+}
+
+function submitIt() {
+  $('form').on('submit', function()){
+    if $("input:first").val() === "correct"){
+    alert('Your form is going to be submitted now.')
+    return;}
+  }
+}
