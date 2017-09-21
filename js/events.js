76e447ae2a2fa9fed249df601d43b3d648134604
Man Leung <lepusleung@gmail.com>
2017-09-21 20:01:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(key) {
-    if(key.which == 71){
+    if(key.which == 82){
         alert('g was pressed');
     }
   });
